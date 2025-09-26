3efeda9d70e4258904a31f08cf6c728ce6a189a2
juanariviano <juanariviano@gmail.com>
2025-09-26 07:14:23 +0700

    update project

--- src/data/projectapps.js
+++ src/data/projectapps.js
@@ -22,4 +22,18 @@ export const projectapps = [
       github: "https://github.com/ronyandrean/LuxBid",
         deploy: "none",
     },
+    {
+      id: 3,
+      name: "Neuronesia",
+      description: "Personalized AI Advisor & Analytic Chatbot",
+      github: "https://github.com/JustKalvin/Neuronesia",
+        deploy: "none",
+    },
+    {
+      id: 4,
+      name: "DiagnoAI",
+      description: "Machine Learning-based application designed to assist in predicting heart disease based on user health data.",
+      github: "https://github.com/juanariviano/DiagnoAI",
+        deploy: "none",
+    },
   ];
